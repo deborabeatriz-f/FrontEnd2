6a851cf1da4e94337abb7551efcd3cfd5b95fc68
Débora Beatriz Figueiró <82326298+deborabeatriz-f@users.noreply.github.com>
2021-10-24 15:13:39 -0300

    Upload js DOM

---
+++ DOM/js/script.js
@@ -0,0 +1,13 @@
+// selecionar/alterar H1
+let nome = prompt("Olá! Qual sei nome? ");
+
+let h1 = (document.querySelector("h1").innerHTML = `Bem vindo(a) ${nome}`);
+
+// selecionar/alterar DIV class=conteudo
+let conteudo = document.getElementsByClassName(conteudo);
+
+// selecionar/alterar SECTION
+let section = document.querySelector(section);
+
+// outros
+document.querySelector(".animado h2").innerHTML = `Animado Porém Cansado`;
